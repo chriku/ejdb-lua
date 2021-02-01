3ec5910a55851a90cc3d0abdb10c76a27b9a7208
Christian Kurz <chrikuvellberg@gmail.com>
2021-02-01 09:24:19 +0100

    unit test for comparing numbers

--- tests.lua
+++ tests.lua
@@ -16,4 +16,16 @@ function test.insert()
   test.equal(seen[2], 1)
   db:close()
 end
+function test.compare()
+  local db = ejdb.open("test.db", "w")
+  assert(db:put("docs", {name = "Doc 1", num = 1}))
+  assert(db:put("docs", [[{"name" :"Doc 2", "num":2}]]))
+  local seen = {}
+  local q = assert(ejdb.query("docs","/[num = :num]"))
+  q:set("num", 0, 2)
+  assert(db:exec(q, function(id, data) seen[data.num] = (seen[data.num] or 0) + 1 end))
+  test.equal(seen[1] or 0, 0)
+  test.equal(seen[2] or 0, 1)
+  db:close()
+end
 test.summary()
